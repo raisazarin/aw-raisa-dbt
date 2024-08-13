19b0a527b66927d33635f51448f942c19b91a03f
Raisa Zarin <raisazarinn@gmail.com>
2024-08-13 22:57:18 +0000

    create stg_customer model

---
+++ models/staging/customers/stg_customer.sql
@@ -0,0 +1,26 @@
+with 
+
+source as (
+
+    select * from {{ source('adventure_works_source', 'customer') }}
+
+)
+
+, transformed as (
+
+    select
+        /* primary key */ 
+        customerid as customer_id
+
+        /* foreign key */
+        , personid as perosn_id
+        , storeid as store_id
+        , territoryid as territory_id
+
+        /* system column */
+        , modifieddate  as updated_at
+    from source
+
+)
+
+select * from transformed
