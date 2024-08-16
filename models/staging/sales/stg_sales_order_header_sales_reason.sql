3e1461d0729547a1facf903510dea8b02ae372eb
Raisa Zarin <raisazarinn@gmail.com>
2024-08-16 19:41:53 +0000

    create stg_sales_order_hearder_reason SQL file and add it to its YML source file

---
+++ models/staging/sales/stg_sales_order_header_sales_reason.sql
@@ -0,0 +1,22 @@
+with 
+
+source as (
+
+    select * from {{ source('adventure_works_source', 'salesorderheadersalesreason') }}
+
+)
+
+, transformed as (
+
+    select
+        /* foreign key */
+        cast(salesorderid as string) as sales_order_id
+        , cast(salesreasonid as string) as sales_reason_id
+
+        /* system column */
+        , modifieddate  as updated_at
+    from source
+
+)
+
+select * from transformed
