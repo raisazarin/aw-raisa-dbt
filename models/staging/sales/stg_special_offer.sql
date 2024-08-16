bdf81af5b0bd0d458b902836aceb90543fdf1059
Raisa Zarin <raisazarinn@gmail.com>
2024-08-16 20:47:18 +0000

    create stg_special_offer SQL file add its source do YML source file

---
+++ models/staging/sales/stg_special_offer.sql
@@ -0,0 +1,30 @@
+with 
+
+source as (
+
+    select * from {{ source('adventure_works_source', 'specialoffer') }}
+
+)
+
+, transformed as (
+
+    select
+        /* primary key */ 
+        cast(specialofferid as string) as special_offer_id
+
+        , description as discount_description
+        , discountpct as discount_percentage
+        , type as discount_category
+        , category as discount_customer_category
+        , startdate as start_offer_date
+        , enddate as end_offer_date
+        , minqty as min_quantity
+        , maxqty as max_quantity
+
+        /* system column */
+        , modifieddate  as updated_at
+    from source
+
+)
+
+select * from transformed
