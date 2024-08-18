6d4d54df6d2999407ea413f005853b83f8405f24
Raisa Zarin <raisazarinn@gmail.com>
2024-08-18 19:42:52 +0000

    create stg_employee SQL file

---
+++ models/staging/sales/stg_employee.sql
@@ -0,0 +1,24 @@
+with 
+
+source as (
+
+    select * from {{ source('adventure_works_source', 'employee') }}
+
+)
+
+, transformed as (
+
+    select
+        /* primary key */ 
+        cast(businessentityid as string) as person_id
+
+        , jobtitle as job_title
+
+        /* system column */
+        , modifieddate  as updated_at
+
+    from source
+
+)
+
+select * from transformed
