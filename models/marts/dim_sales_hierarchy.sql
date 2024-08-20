a49eef2cef3a957f31671614d6a1de6a38e3350d
Raisa Zarin <raisazarinn@gmail.com>
2024-08-20 20:46:59 +0000

    fix indentation to dim_sales_hierarchy

--- models/marts/dim_sales_hierarchy.sql
+++ models/marts/dim_sales_hierarchy.sql
@@ -1,28 +1,21 @@
 with 
-
-int_sales_hierarchy as (
-
-    select * from {{ ref('int_sales_hierarchy') }}
-
+    int_sales_hierarchy as (
+        select *
+        from {{ ref('int_sales_hierarchy') }}
 )
 
-, dim_sales_hierarchy as (
+, generate_sk as (
     select
-        /* surrogate key */
         {{ dbt_utils.generate_surrogate_key(['salesperson_id']) }} as sales_hierarchy_sk
-
-        /* primary key */
         , salesperson_id
-
         , territory_id
         , territory_name
         , country_region_id
         , group_region
-
-        /* system column */
         , source_updated_at
         , current_timestamp() as updated_at
       from int_sales_hierarchy
 )
 
-select * from dim_sales_hierarchy
+select *
+from generate_sk
