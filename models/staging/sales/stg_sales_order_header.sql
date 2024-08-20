f05244731d47c0a6da54a74e296a1cb337ece5dd
Raisa Zarin <raisazarinn@gmail.com>
2024-08-20 19:21:18 +0000

    fix indentation and change shipment_address_id name columns for stg_sales_order_header

--- models/staging/sales/stg_sales_order_header.sql
+++ models/staging/sales/stg_sales_order_header.sql
@@ -1,24 +1,20 @@
 with 
-
-source as (
-
-    select * from {{ source('adventure_works_source', 'salesorderheader') }}
-
+    source as (
+        select *
+        from {{ source('adventure_works_source', 'salesorderheader') }}
 )
 
 , transformed as (
-
     select
         /* primary key */ 
         cast(salesorderid as string) as sales_order_id
-
         /* foreign key */
         , cast(customerid as string) as customer_id
         , cast(salespersonid as string) as salesperson_id
         , cast(territoryid as string) as territory_id
         , cast(creditcardid as string) as creditcard_id
-        , cast(shiptoaddressid as string) as address_id
-
+        , cast(shiptoaddressid as string) as shipment_address_id
+        /* other columns */
         , orderdate as order_date
         , duedate as due_date
         , shipdate as ship_date
@@ -27,11 +23,10 @@ source as (
         , taxamt as tax_amount
         , freight as freight
         , totaldue as total_due
-
         /* system column */
         , modifieddate  as updated_at
     from source
-
 )
 
-select * from transformed
+select *
+ from transformed
