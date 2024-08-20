edc445915accb7e88d6e885af603f67a6aedb2c4
Raisa Zarin <raisazarinn@gmail.com>
2024-08-20 19:36:27 +0000

    fix indentation and change de column of shipment_address_id for fact_sales

--- models/marts/fact_sales.sql
+++ models/marts/fact_sales.sql
@@ -1,28 +1,19 @@
 with 
-
-int_sales as (
-
-    select * from {{ ref('int_sales') }}
-
+    int_sales as (
+        select * 
+        from {{ ref('int_sales') }}
 )
 
-, fact_sales as (
+, generate_sk as (
     select
-        /* surrogate key */
         {{ dbt_utils.generate_surrogate_key(['sales_order_detail_id']) }} as sales_sk
-
-        /* primary key */
-        , sales_order_detail_id
-    
-        /* foreign key */
         , {{ dbt_utils.generate_surrogate_key(['product_id']) }} as product_fk
         , {{ dbt_utils.generate_surrogate_key(['special_offer_id']) }} as special_offer_fk
         , {{ dbt_utils.generate_surrogate_key(['customer_id']) }} as customer_fk
         , {{ dbt_utils.generate_surrogate_key(['address_id']) }} as distribution_fk
         , {{ dbt_utils.generate_surrogate_key(['salesperson_id']) }} as sales_hierarchy_fk
         , sales_order_id
-        , address_id as shipment_address_id
-
+        , shipment_address_id
         , payment_method
         , order_quantity
         , unit_price
@@ -32,11 +23,10 @@ int_sales as (
         , due_date
         , ship_date
         , is_first_order
-
-        /* system column */
         , source_updated_at
         , current_timestamp() as updated_at
       from int_sales
 )
 
-select * from fact_sales
+select *
+from generate_sk
