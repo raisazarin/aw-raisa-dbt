4ea41af40eb6b2345d310177fa1316818371e307
Raisa Zarin <raisazarinn@gmail.com>
2024-08-20 21:45:43 +0000

    fix indentation to products staging models

--- models/staging/products/stg_product.sql
+++ models/staging/products/stg_product.sql
@@ -1,20 +1,15 @@
 with 
-
-source as (
-
-    select * from {{ source('adventure_works_source', 'product') }}
-
+    source as (
+        select *
+        from {{ source('adventure_works_source', 'product') }}
 )
 
 , transformed as (
-
     select
         /* primary key */ 
         cast(productid as string) as product_id
-
         /* foreign key */
         , cast(productsubcategoryid as string) as product_sub_category_id
-
         , name as product_name
         , finishedgoodsflag as finished_goods_flag
         , standardcost as standard_cost
@@ -22,11 +17,10 @@ source as (
         , productline as product_line
         , sellstartdate as sell_start_date
         , sellenddate as sell_end_date
-
         /* system column */
         , modifieddate  as updated_at
     from source
-
 )
 
-select * from transformed
+select *
+from transformed
--- models/staging/products/stg_product_sub_category.sql
+++ models/staging/products/stg_product_sub_category.sql
@@ -1,26 +1,20 @@
 with 
-
-source as (
-
-    select * from {{ source('adventure_works_source', 'productsubcategory') }}
-
+    source as (
+        select *
+        from {{ source('adventure_works_source', 'productsubcategory') }}
 )
 
 , transformed as (
-
     select
         /* primary key */ 
         cast(productsubcategoryid as string) as product_sub_category_id
-
          /* foreign key */
         , cast(productcategoryid as string) as product_category_id
-
         , name as product_sub_category_name
-
         /* system column */
         , modifieddate  as updated_at
     from source
-
 )
 
-select * from transformed
+select *
+from transformed
