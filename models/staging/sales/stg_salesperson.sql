ceccf4fe68a2148e10f980e30958a9a02a1995aa
Raisa Zarin <raisazarinn@gmail.com>
2024-08-20 20:40:22 +0000

    fix stg_salesperson indentation

--- models/staging/sales/stg_salesperson.sql
+++ models/staging/sales/stg_salesperson.sql
@@ -1,24 +1,19 @@
 with 
-
-source as (
-
-    select * from {{ source('adventure_works_source', 'salesperson') }}
-
+    source as (
+        select *
+        from {{ source('adventure_works_source', 'salesperson') }}
 )
 
 , transformed as (
-
     select
         /* primary key */ 
         cast(businessentityid as string) as salesperson_id
-
         /* foreign key */
         , cast(territoryid as string) as territory_id
-
         /* system column */
         , modifieddate  as updated_at
     from source
-
 )
 
-select * from transformed
+select *
+from transformed
